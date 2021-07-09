b63f95ccac98ecaece9ab89ea6996acf176a537a
Tyler Ware <tyler.ware.777@gmail.com>
2021-07-09 13:24:35 -0600

    Fixing bug where output buffer isn't as expected

--- elpm.el
+++ elpm.el
@@ -36,32 +36,32 @@ If DIRECTORY is not defined then `elpm-directory' is used.
 Note that this also loads all previously installed packages in DIRECTORY."
   (let ((p
          (funcall (if wait #'elpm--sync #'elpm--async)
-          `(progn
-             (require 'elpm)
-             (require 'info)
-             (let ((elpm-directory ,(or directory elpm-directory))
-                   (original-load-path load-path)
-                   (original-Info-directory-list Info-directory-list))
+                  `(progn
+                     (require 'elpm)
+                     (require 'info)
+                     (let ((elpm-directory ,(or directory elpm-directory))
+                           (original-load-path load-path)
+                           (original-Info-directory-list Info-directory-list))
 
-               ;; Bootstrap straight
-               (elpm--straight)
-               ;; Make sure we have all the current packages
-               (elpm-use-packages--intern (plist-get
-                                           (elpm-get-packages-plist)
-                                           :recipes))
-               ;; Add new recipes
-               (elpm-use-packages--intern ',recipes)
-               ;; Take the diff of the load path to return to the parent process
-               (message
-                "##elpm-details:begin##
+                       ;; Bootstrap straight
+                       (elpm--straight)
+                       ;; Make sure we have all the current packages
+                       (elpm-use-packages--intern (plist-get
+                                                   (elpm-get-packages-plist)
+                                                   :recipes))
+                       ;; Add new recipes
+                       (elpm-use-packages--intern ',recipes)
+                       ;; Take the diff of the load path to return to the parent process
+                       (message
+                        "##elpm-details:begin##
 %s
 ##elpm-details:end##"
-                (prin1-to-string
-                 (list :load-path (seq-difference load-path original-load-path)
-                       :info-directory-list (seq-difference Info-directory-list original-Info-directory-list))))))
-          #'(lambda (process &optional _ignore)
-              (when (memq (process-status process) '(exit signal))
-                (elpm--process-output-buffer (process-buffer process)))))))
+                        (prin1-to-string
+                         (list :load-path (seq-difference load-path original-load-path)
+                               :info-directory-list (seq-difference Info-directory-list original-Info-directory-list))))))
+                  #'(lambda (process &optional _ignore)
+                      (when (memq (process-status process) '(exit signal))
+                        (elpm--process-output-buffer (process-buffer process)))))))
 
     (if (bufferp p)
         (elpm--process-output-buffer p)
@@ -84,11 +84,22 @@ Note that this also loads all previously installed packages in DIRECTORY."
 This loads values into `load-path'and into `Info-directory-list'.
 "
   (let* ((details (with-current-buffer buffer 
-                    (goto-char (point-min))
-                    (while (re-search-forward "^##elpm-details:\\(begin\\|end\\)##$" nil t)
-                      (kill-whole-line))
-                    (goto-char (point-min))
-                    (car (read-from-string (buffer-string)))))
+                    (save-excursion
+                      (save-match-data
+                        (goto-char (point-min))
+                        (let ((start
+                               (when (re-search-forward "^##elpm-details:begin##$" nil t)
+                                 (forward-line)
+                                 (beginning-of-line)
+                                 (point)))
+                              (end
+                               (when (re-search-forward "^##elpm-details:end##$" nil t)
+                                 (forward-line -1)
+                                 (end-of-line)
+                                 (point))))
+
+                          (when (and start end)
+                            (car (read-from-string (buffer-substring start end)))))))))
          (load-path-additions (plist-get details :load-path))
          (info-directory-list-additions (plist-get details :info-directory-list)))
 
@@ -112,7 +123,7 @@ indicates that we are reading data from the subprocess (specifically lisp data)"
   "Load / install a RECIPE in the DIRECTORY.
 
 See `elpm-use-packages' for more."
-  (elpm-use-packages (list recipe) directory))
+  (elpm-use-packages (list recipe) directory t))
 
 (defun elpm-use-all (&optional directory)
   "Load all packages previously installed in the DIRECTORY.
@@ -123,15 +134,15 @@ See `elpm-use-packages' for more."
 (defun elpm-get-packages-file-name (&optional directory)
   "Get the packages file for the DIRECTORY."
   (expand-file-name "elpm-packages.el" (or directory
-                                  elpm-directory)))
+                                           elpm-directory)))
 
 (defun elpm-get-packages-plist (&optional directory)
   "Get the packages plist from the packages file in the DIRECTORY."
   (let* ((file-name (elpm-get-packages-file-name directory))
          (file-contents (when (file-exists-p file-name)
-                           (with-temp-buffer
-                             (insert-file-contents file-name)
-                             (buffer-string))))
+                          (with-temp-buffer
+                            (insert-file-contents file-name)
+                            (buffer-string))))
          (packages-data (and file-contents
                              (not (equal file-contents ""))
                              (car (read-from-string file-contents)))))
@@ -160,7 +171,7 @@ See `elpm-use-packages' for more."
 (defun elpm--new-packages-plist ()
   "Generate a new packages plist."
   `(:version ,elpm-version
-    :recipes ()))
+             :recipes ()))
 
 (defun elpm--async (sexp sentinel-fn)
   "Execute SEXP asyncronously in a new instance of Emacs.
@@ -189,19 +200,20 @@ Returns the process."
 This allows for a sandbox environment."
   (message "Running...")
   (let ((buf (generate-new-buffer " *elpm-sync*")))
-         (call-process
-          ;; emacs binary
-          (file-truename
-           (expand-file-name invocation-name
-                             invocation-directory))
-          nil
-          buf
-          nil
-          "-Q"
-          "--batch"
-          "--eval"
-          (prin1-to-string sexp))
-         buf))
+    (pop-to-buffer buf)
+    (call-process
+     ;; emacs binary
+     (file-truename
+      (expand-file-name invocation-name
+                        invocation-directory))
+     nil
+     buf
+     t 
+     "-Q"
+     "--batch"
+     "--eval"
+     (prin1-to-string sexp))
+    buf))
 
 (defun elpm--straight ()
   "Bootstrap straight.el."
@@ -222,16 +234,16 @@ This allows for a sandbox environment."
 (defun elpm-update-recipes (&optional directory)
   (interactive)
   (let ((p (elpm--sync
-   `(progn
-      (require 'elpm)
-      (let ((elpm-directory ,(or directory elpm-directory))
-            (original-load-path load-path))
+            `(progn
+               (require 'elpm)
+               (let ((elpm-directory ,(or directory elpm-directory))
+                     (original-load-path load-path))
 
-        ;; Bootstrap straight
-        (elpm--straight)
-        (let ((user-emacs-directory elpm-directory))
-          )))
-   #'(lambda (a b) nil))))
+                 ;; Bootstrap straight
+                 (elpm--straight)
+                 (let ((user-emacs-directory elpm-directory))
+                   )))
+            #'(lambda (a b) nil))))
 
     (set-process-filter p (lambda (_p output)
                             (message (replace-regexp-in-string "\n$" "" output))))))
@@ -239,10 +251,10 @@ This allows for a sandbox environment."
 (defmacro elpm--make-simple-command (command-name &rest sexp)
   `(defun ,(intern (concat "elpm-" (symbol-name command-name)))
        (&optional directory)
-       (interactive)
-       (let* ((sexp ',sexp)
-              (p (elpm--async
-                 `(progn
+     (interactive)
+     (let* ((sexp ',sexp)
+            (p (elpm--async
+                `(progn
                    (require 'elpm)
                    (let ((elpm-directory ,(or directory elpm-directory))
                          (original-load-path load-path))
@@ -251,12 +263,12 @@ This allows for a sandbox environment."
                      (elpm--straight)
                      (let ((user-emacs-directory elpm-directory))
                        ,@sexp)))
-                 #'(lambda (a b) nil))))
+                #'(lambda (a b) nil))))
 
-         (set-process-filter p (lambda (_p output)
-                                 (message (replace-regexp-in-string "\n$" "" output))))
+       (set-process-filter p (lambda (_p output)
+                               (message (replace-regexp-in-string "\n$" "" output))))
 
-         p)))
+       p)))
 
 
 (elpm--make-simple-command update-recipes 
